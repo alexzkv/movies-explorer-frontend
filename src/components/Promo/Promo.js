9ce25800703b68220c35e3ed30b27542a849cf04
Захаров Александр Александрович <AlekAleZakharov@vimpelcom.ru>
2022-10-22 23:16:22 +0300

    feat: add promo component

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -1,11 +1,14 @@
 import './Promo.css';
+import ball from '../../images/ball.svg';
 
 export default function Promo() {
   return (
     <section className='promo'>
-      <h1 className='promo__title'>Учебный проект студента факультета Веб-разработки.</h1>
-      <h2 className='promo__subtitle'>Листайте ниже, чтобы узнать больше про этот проект и его создателя.</h2>
-      <img alt='шар из слов' className='promo__img' />
+      <div className='promo__info'>
+        <h1 className='promo__title'>Учебный проект студента факультета Веб-разработки.</h1>
+        <p className='promo__subtitle'>Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
+      </div>
+      <img src={ball} alt='шар из слов' className='promo__img' />
     </section>
   );
 }
