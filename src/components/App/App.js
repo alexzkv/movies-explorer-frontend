d783f62f5d95a2a52473c095769ce32b431ee462
Захаров Александр Александрович <AlekAleZakharov@vimpelcom.ru>
2022-10-26 23:23:58 +0300

    feat: add footer component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import Header from '../Header/Header';
 import Main from '../Main/Main';
+import Footer from '../Footer/Footer';
 
 export default function App() {
   return (
     <>
       <Header />
       <Main />
+      <Footer />
     </>
   )
 }
